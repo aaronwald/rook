26ed89caf0456b4652ba3dfa52d83c7bf700291b
Aaron Wald <aaronwald@gmail.com>
2024-12-13 01:44:30 +0000

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module magpie/hello
+
+go 1.23.2
+
+require rsc.io/quote v1.5.2
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ hello.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	opts := MQTT.NewClientOptions().AddBroker("tcp://homeassistant.local:1883")
+	opts.SetClientID("go_mqtt_client")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.SetUsername("mqtt")
+	opts.SetPassword("mqtt")
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	sub(client)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	fmt.Println("Waiting for Ctrl+C...")
+	<-c
+	fmt.Println("\nCtrl+C received. Exiting gracefully...")
+
+	client.Disconnect(250)
+
+}
+
+var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+func sub(client MQTT.Client) {
+	topic := "#"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s\n", topic)
+}
