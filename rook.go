a5546187a4f67669a7d2166ef905631d8c9dbef4
Aaron Wald <aaronwald@gmail.com>
2024-12-17 22:31:41 +0000

    parse motion

--- rook.go
+++ rook.go
@@ -14,12 +14,20 @@ import (
 	// LO "github.com/samber/lo"
 )
 
+var motion_map map[string]int
+
 // Define a struct that matches the JSON payload structure
+// {"encryption":false,"BTHome_version":2,"pid":198,"Battery":100,"Illuminance":0,"Motion":1,"addr":"e8:e0:7e:a6:ac:db","rssi":-56}
+
 type Payload struct {
-	// Add fields according to your JSON structure
-	Time  string `json:"time"`
-	Model string `json:"model"`
-	// Add more fields as needed
+	Encryption    bool   `json:"encryption"`
+	BTHomeVersion int    `json:"BTHome_version"`
+	Pid           int    `json:"pid"`
+	Battery       int    `json:"Battery"`
+	Illuminance   int    `json:"Illuminance"`
+	Motion        int    `json:"Motion"`
+	Addr          string `json:"addr"`
+	Rssi          int    `json:"rssi"`
 }
 
 type Status struct {
@@ -36,12 +44,29 @@ var context *Context
 // https://github.com/samber/lo
 // https://github.com/gin-gonic/gin - need so we can front w/ envoy
 // https://github.com/avelino/awesome-go
+// https://gobyexample.com/
+
+func vard(x ...int) (int, error) {
+	var y int
+	for _, v := range x {
+		y += v
+	}
+	return y, nil
+}
 
 func main() {
+	var z, err = vard(1, 2, 3)
+	if err == nil {
+		fmt.Println(z)
+	}
 	context = &Context{MessageCount: 0}
 	username := flag.String("username", "foo", "MQTT username")
 	password := flag.String("password", "bar", "MQTT password")
+	mqtt_hostname := flag.String("mqtt_server", "homeassistant.local", "MQTT hostname")
+	mqtt_port := flag.Int("mqtt_port", 1883, "MQTT port")
 	flag.Parse()
+
+	motion_map = make(map[string]int)
 
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -49,7 +74,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	opts := MQTT.NewClientOptions().AddBroker("tcp://homeassistant.local:1883")
+	opts := MQTT.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s:%d", *mqtt_hostname, *mqtt_port))
 	opts.SetClientID("rook_" + hostname)
 	opts.SetDefaultPublishHandler(messagePubHandler)
 	opts.SetUsername(*username)
@@ -114,11 +139,19 @@ var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Me
 
 	// Use the parsed data
 	fmt.Printf("Parsed payload: %+v\n", payload)
+	fmt.Printf("\tMotion: %d\n", payload.Motion)
+
+	val, ok := motion_map[msg.Topic()]
+	if (ok && val != payload.Motion) || !ok {
+		fmt.Print("Send email\n")
+	}
+
+	motion_map[msg.Topic()] = payload.Motion
 }
 
 // TODO Parse motion detection and send email
 func sub(client MQTT.Client) {
-	topic := "#"
+	topic := "mostert/motion/#"
 	token := client.Subscribe(topic, 1, nil)
 	token.Wait()
 	fmt.Printf("Subscribed to topic: %s\n", topic)
