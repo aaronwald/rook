4887371c9290449cce0f14e710c4b7cf9f282a42
Aaron Wald <aaronwald@gmail.com>
2024-12-18 00:52:47 +0000

    latest

--- go.mod
+++ go.mod
@@ -12,5 +12,6 @@ require (
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
--- rook.go
+++ rook.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"net/smtp"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/gorilla/mux"
-	// LO "github.com/samber/lo"
 )
 
 var motion_map map[string]int
+var gmail_password string
+var gmail_username string
 
 // Define a struct that matches the JSON payload structure
 // {"encryption":false,"BTHome_version":2,"pid":198,"Battery":100,"Illuminance":0,"Motion":1,"addr":"e8:e0:7e:a6:ac:db","rssi":-56}
@@ -40,31 +45,30 @@ type Context struct {
 
 var context *Context
 
-// https://gorilla.github.io/
-// https://github.com/samber/lo
-// https://github.com/gin-gonic/gin - need so we can front w/ envoy
 // https://github.com/avelino/awesome-go
 // https://gobyexample.com/
 
-func vard(x ...int) (int, error) {
-	var y int
-	for _, v := range x {
-		y += v
-	}
-	return y, nil
-}
-
 func main() {
-	var z, err = vard(1, 2, 3)
-	if err == nil {
-		fmt.Println(z)
-	}
 	context = &Context{MessageCount: 0}
 	username := flag.String("username", "foo", "MQTT username")
 	password := flag.String("password", "bar", "MQTT password")
 	mqtt_hostname := flag.String("mqtt_server", "homeassistant.local", "MQTT hostname")
 	mqtt_port := flag.Int("mqtt_port", 1883, "MQTT port")
+	gmail_username_file := flag.String("gmail_username_file", "gmail_username.txt", "Gmail username file")
+	gmail_password_file := flag.String("gmail_password_file", "gmail_password.txt", "Gmail password file")
 	flag.Parse()
+
+	dat, err := os.ReadFile(*gmail_username_file)
+	if err != nil {
+		panic(err)
+	}
+	gmail_username = strings.TrimSpace(string(dat))
+
+	dat2, err2 := os.ReadFile(*gmail_password_file)
+	if err != nil {
+		panic(err2)
+	}
+	gmail_password = strings.TrimSpace(string(dat2))
 
 	motion_map = make(map[string]int)
 
@@ -126,7 +130,8 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	log.Printf("DEBUG: TOPIC: %s\n", msg.Topic())
+	log.Printf("DEBUG: MSG: %s\n", msg.Payload())
 	context.MessageCount++
 
 	// Parse the JSON payload
@@ -144,6 +149,14 @@ var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Me
 	val, ok := motion_map[msg.Topic()]
 	if (ok && val != payload.Motion) || !ok {
 		fmt.Print("Send email\n")
+		body := "Motion detected"
+		if payload.Motion == 0 {
+			body = "Motion cleared"
+		}
+		err = sendEmail(gmail_username, msg.Topic(), body)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	motion_map[msg.Topic()] = payload.Motion
@@ -155,4 +168,70 @@ func sub(client MQTT.Client) {
 	token := client.Subscribe(topic, 1, nil)
 	token.Wait()
 	fmt.Printf("Subscribed to topic: %s\n", topic)
+}
+
+func sendEmail(to, subject, body string) error {
+	from := gmail_username
+	password := gmail_password
+
+	// Gmail SMTP server address
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "465"
+
+	// Message
+	message := []byte("Subject: " + subject + "\r\n" +
+		"\r\n" + body + "\r\n")
+
+	// Authentication
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// TLS config
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         smtpHost,
+	}
+
+	// Connect to the SMTP server
+	conn, err := tls.Dial("tcp", smtpHost+":"+smtpPort, tlsconfig)
+	if err != nil {
+		return fmt.Errorf("failed to connect to SMTP server: %w", err)
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, smtpHost)
+	if err != nil {
+		return fmt.Errorf("failed to create SMTP client: %w", err)
+	}
+	// Authenticate
+	if err = client.Auth(auth); err != nil {
+		return fmt.Errorf("failed to authenticate: %w", err)
+	}
+
+	// Set the sender and recipient
+	if err = client.Mail(from); err != nil {
+		return fmt.Errorf("failed to set sender: %w", err)
+	}
+	if err = client.Rcpt(to); err != nil {
+		return fmt.Errorf("failed to set recipient: %w", err)
+	}
+
+	// Send the email body
+	w, err := client.Data()
+	if err != nil {
+		return fmt.Errorf("failed to send email body: %w", err)
+	}
+	_, err = w.Write(message)
+	if err != nil {
+		return fmt.Errorf("failed to write email body: %w", err)
+	}
+	err = w.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close email body: %w", err)
+	}
+
+	// Close the connection
+	client.Quit()
+
+	fmt.Println("Email sent successfully")
+	return nil
 }
